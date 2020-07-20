1bf78f0c1b5fd3ea75ce6cb0d5a70577179af1f2
misitebao <i@misitebao.com>
2020-07-20 12:09:05 +0800

    add:添加TODO 新增about示例页面 新增配置文件

--- config/config.js
+++ config/config.js
@@ -2,4 +2,16 @@
  * @Author       : MS
  * @LastEditors  : MS
  * @Description  : 配置文件
- */ 
+ */
+
+module.exports = {
+    dev: {
+        BasePath: 'dev.com',
+    },
+    test: {
+        BasePath: 'test.com',
+    },
+    prod: {
+        BasePath: 'prod.com',
+    },
+};
